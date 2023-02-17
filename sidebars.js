1aab93e61e6031736c9ffe7ed193f605fec13dbe
kolik <55196465+kolikiscool@users.noreply.github.com>
2023-02-17 15:46:46 +0100

    added visualizer early

--- sidebars.js
+++ sidebars.js
@@ -1,16 +1,13 @@
-  // Get the sidebars
-  const leftSidebar = document.querySelector('.sidebar.left');
-  const rightSidebar = document.querySelector('.sidebar.right');
-  const player = document.querySelector('.content');
-  
-  // Hide sidebars when mouse is in player part
-  player.addEventListener('mouseenter', function() {
-    leftSidebar.classList.remove('visible');
-    rightSidebar.classList.remove('visible');
-  });
-  
-  // Show sidebars when mouse is outside of player part
-  player.addEventListener('mouseleave', function() {
-    leftSidebar.classList.add('visible');
-    rightSidebar.classList.add('visible');
-  });
+const leftSidebar = document.querySelector(".sidebar.left");
+const rightSidebar = document.querySelector(".sidebar.right");
+const player = document.querySelector(".content");
+
+player.addEventListener("mouseenter", function () {
+  leftSidebar.classList.remove("visible");
+  rightSidebar.classList.remove("visible");
+});
+
+player.addEventListener("mouseleave", function () {
+  leftSidebar.classList.add("visible");
+  rightSidebar.classList.add("visible");
+});
